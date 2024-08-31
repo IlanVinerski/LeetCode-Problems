18587b97fb48d2307971f74d4495a8a6352cd10e
IlanVinerski <98633717+IlanVinerski@users.noreply.github.com>
2024-08-31 13:30:30 +0300

    Time: 5 ms (97.91%), Space: 45.4 MB (6.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,43 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        int a,b;
-		Stack<Integer> S = new Stack<Integer>();
-		for (String s : tokens) {
-			if(s.equals("+")) {
-				S.add(S.pop()+S.pop());
-			}
-			else if(s.equals("/")) {
-				b = S.pop();
-				a = S.pop();
-				S.add(a / b);
-			}
-			else if(s.equals("*")) {
-				S.add(S.pop() * S.pop());
-			}
-			else if(s.equals("-")) {
-				b = S.pop();
-				a = S.pop();
-				S.add(a - b);
-			}
-			else {
-				S.add(Integer.parseInt(s));
-			}
-		}	
-		return S.pop();
+         if (tokens == null || tokens.length == 0) {
+            return -1;
+        }
+
+        int first;
+        int second;
+        final Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+
+                case "-":
+                    first = stack.pop();
+                    second = stack.pop();
+
+                    stack.push(second - first);
+                    break;
+
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+
+                case "/":
+                    first = stack.pop();
+                    second = stack.pop();
+
+                    stack.push(second / first);
+                    break;
+
+                default:
+                    stack.push(Integer.parseInt(token));
+                    break;
+            }
+        }
+
+        return stack.pop();
     }
 }

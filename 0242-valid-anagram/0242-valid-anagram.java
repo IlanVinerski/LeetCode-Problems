121e8dd074e5417fc0ff105bbd0cb9eca43e581b
IlanVinerski <98633717+IlanVinerski@users.noreply.github.com>
2024-08-31 13:13:18 +0300

    Time: 3 ms (89.03%), Space: 43.4 MB (64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-      int[] numbers = new int[26];
+       int[] numbers = new int[26];
 
         if(s.length()!=t.length()){
             return false;

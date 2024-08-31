fb439b51146e2290eb5abbd2a272f3b52a3767a8
IlanVinerski <98633717+IlanVinerski@users.noreply.github.com>
2024-08-31 13:11:19 +0300

    Time: 2 ms (97.31%), Space: 43.1 MB (72.01%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-        //if the lengths are not equal it cant be anagram
-        if(s.length() != t.length())
-            return false;
-        
-        // Create a HashMap to count characters
-        HashMap<Character, Integer> map = new HashMap<>();
+         int[] numbers = new int[26];
 
-        //counet each char in the string s
-        for(char c : s.toCharArray())
-            map.put(c, map.getOrDefault(c,0)+1);
-        
-        // Subtract the count for each character in string `t`
-        for(char c : t.toCharArray()){
-            if(!map.containsKey(c))
-                return false; //if 't' has char that not is 's'
-            
-            //we substurct 1 from the value of key c
-            map.put(c, map.get(c)-1);
-            
-            // If `t` has more of a character c than `s`
-            if(map.get(c)<0)
-                return false;
+        if(s.length()!=t.length()){
+            return false;
         }
 
-        return true; //we have an anagram!
+        for(char ch: s.toCharArray()){
+            numbers[ch-97]++;
+        }
+        for(char ch: t.toCharArray()){
+            if(numbers[ch-97]>0){
+                numbers[ch-97]--;
+            } else{
+                return false;
+            }
+        }
+        return true;
     }
 }
+

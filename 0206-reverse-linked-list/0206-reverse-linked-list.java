1396e86f4de8ac275674f64f11ed63ea5731a078
IlanVinerski <98633717+IlanVinerski@users.noreply.github.com>
2024-09-01 13:05:20 +0300

    Time: 0 ms (100%), Space: 42.3 MB (53.3%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        
+
         ListNode current = head;
         ListNode prev = null;
         ListNode temp;
@@ -24,3 +24,6 @@ class Solution {
         return prev;
     }
 }
+
+//Time: O(n) when n is the number of nodes
+//Place: O(1)
